f580e776e67d5e274c795ba400d7fa441e3d75b4
Raul <raulbc099@gmail.com>
2020-04-12 12:23:59 +0300

    lab3 file

--- Lab2/src/main/java/repository/AbstractCRUDRepository.java
+++ Lab2/src/main/java/repository/AbstractCRUDRepository.java
@@ -32,7 +32,9 @@ public abstract class AbstractCRUDRepository<ID, E extends HasID<ID>> implements
     public E save(E entity) throws ValidationException {
         try {
             validator.validate(entity);
-            entities.putIfAbsent(entity.getID(), entity);
+            E current=entities.putIfAbsent(entity.getID(), entity);
+            if(current!=null)
+                return null;
             return entity;
         }
         catch (ValidationException ve) {
