66377dadb285dfc65a83cd58c4de91c7f4253033
alexcernov <cernovalex1@gmail.com>
2020-04-12 19:01:48 +0300

    lab 3 done

---
+++ Lab2/src/test/java/TestSaveTemaDeadlineValidator.java
@@ -0,0 +1,66 @@
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Before;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.Assert.*;
+
+//
+public class TestSaveTemaDeadlineValidator {
+
+    private Service service;
+
+
+
+    @Test
+    public void tc_1(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+
+        int done=service.saveTema("aaa","description1",15,4);
+
+        assertEquals(0,done);
+
+    }
+
+    @Test
+    public void tc_2(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+
+        int done=service.saveTema("123","description1",10,0);
+
+
+        assertEquals(0,done);
+    }
+
+
+}
+
